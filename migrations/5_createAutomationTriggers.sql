7224a1f5ec92adf0a5b8ee62eb73efb6f203417e
marmag0 <mikolaj.mazur05@gmail.com>
2026-01-13 00:10:57 +0100

    functions and triggers script division

--- migrations/5_createAutomationTriggers.sql
+++ migrations/5_createAutomationTriggers.sql
@@ -1,4 +1,4 @@
--- migration script to create functions and triggers
+-- migration script to create functions and triggers for automation
 
 -- * -- * -- * -- * --
 
@@ -76,74 +76,9 @@ AFTER UPDATE ON public.orders
 FOR EACH ROW
 EXECUTE FUNCTION public.handle_order_cancellation();
 
--- -- -- -- -- -- -- --
-
--- trigger to check if stock is sufficient before inserting order items
-CREATE OR REPLACE FUNCTION public.check_stock_before_insert()
-RETURNS TRIGGER AS $$
-BEGIN
-    IF (SELECT stock_quantity FROM public.products WHERE product_id = NEW.product_id) < NEW.quantity THEN
-        RAISE EXCEPTION 'Insufficient stock for product_id: %', NEW.product_id;
-    END IF;
-    RETURN NEW;
-END;
-$$ LANGUAGE plpgsql;
-
-CREATE TRIGGER trg_check_stock_before_insert
-BEFORE INSERT ON public.order_items
-FOR EACH ROW
-EXECUTE FUNCTION public.check_stock_before_insert();
-
--- -- -- -- -- -- -- --
-
--- trigger to check if deleted customer has active orders
-CREATE OR REPLACE FUNCTION public.check_active_orders_before_customer_delete()
-RETURNS TRIGGER AS $$
-BEGIN
-    IF EXISTS (
-        SELECT 1 FROM public.orders 
-        WHERE customer_id = OLD.customer_id 
-        AND status NOT IN ('delivered', 'canceled')
-    ) THEN
-        RAISE EXCEPTION 'Cannot delete customer with active orders.';
-    END IF;
-    RETURN OLD;
-END;
-$$ LANGUAGE plpgsql;
-
-CREATE TRIGGER prevent_customer_delete_with_active_orders
-BEFORE DELETE ON public.customers
-FOR EACH ROW
-EXECUTE FUNCTION public.check_active_orders_before_customer_delete();
-
--- -- -- -- -- -- -- --
-
--- trigger prevent deleting address if it is used in active orders
-CREATE OR REPLACE FUNCTION public.check_active_orders_before_address_delete()
-RETURNS TRIGGER AS $$
-BEGIN
-    IF EXISTS (
-        SELECT 1 FROM public.orders 
-        WHERE address_id = OLD.address_id 
-        AND status NOT IN ('delivered', 'canceled')
-    ) THEN
-        RAISE EXCEPTION 'Cannot delete address associated with active orders.';
-    END IF;
-    RETURN OLD;
-END;
-$$ LANGUAGE plpgsql;
-
-CREATE TRIGGER prevent_address_delete_with_active_orders
-BEFORE DELETE ON public.addresses
-FOR EACH ROW
-EXECUTE FUNCTION public.check_active_orders_before_address_delete();
-
 -- * -- * -- * -- * --
 
 -- migrate:down
-
-DROP TRIGGER IF EXISTS prevent_address_delete_with_active_orders ON public.addresses;
-DROP FUNCTION IF EXISTS public.check_active_orders_before_address_delete();
 
 DROP TRIGGER IF EXISTS trg_decrease_stock_after_insert ON public.order_items;
 DROP FUNCTION IF EXISTS public.decrease_stock_on_order_item_insert();
@@ -153,12 +88,6 @@ DROP FUNCTION IF EXISTS public.handle_order_cancellation();
 
 DROP TRIGGER IF EXISTS trg_restore_stock_after_delete ON public.order_items;
 DROP FUNCTION IF EXISTS public.restore_stock_on_order_item_delete();
-
-DROP TRIGGER IF EXISTS prevent_customer_delete_with_active_orders ON public.customers;
-DROP FUNCTION IF EXISTS public.check_active_orders_before_customer_delete();
-
-DROP TRIGGER IF EXISTS trg_check_stock_before_insert ON public.order_items;
-DROP FUNCTION IF EXISTS public.check_stock_before_insert();
 
 DROP TRIGGER IF EXISTS trg_update_updated_at ON public.products;
 DROP FUNCTION IF EXISTS public.update_updated_at();
